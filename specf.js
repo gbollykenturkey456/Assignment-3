0aea02d5ae15c0e42ea8b2b2c5297fc7108bd275
Gbolly <gbolahanakins7@gmail.com>
2022-10-22 17:37:17 +0100

    My Third Assignment submitted

---
+++ specf.js
@@ -0,0 +1,21 @@
+
+const AbsolNumber = alert ("Write a javascript program to compute the absolute difference between specified number and 19 Returns triple their absolute difference If the specified number is greater than 19");
+
+
+function SpecNumber (p) { 
+    
+
+    if (p > 19) {
+        
+        return 3 * (p - 19);
+    }
+
+    else {
+        
+        return (p - 19);
+
+    }
+    
+}
+
+document.write  (SpecNumber (22) );
